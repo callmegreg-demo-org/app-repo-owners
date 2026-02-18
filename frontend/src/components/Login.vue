f8b3d566a305bce90318dd72578550e2c64753c0
Greg Mohler <110078080+CallMeGreg@users.noreply.github.com>
2026-02-18 12:04:28 -0500

    Add console log for error logging in Login component

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -29,6 +29,7 @@ export default {
   name: "Login",
   computed: {
     hasError() {
+      console.log("error logging")
       const params = new URLSearchParams(window.location.search)
       return params.has('error')
     },
